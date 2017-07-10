75cc5b0b721ac755d37bc66dd0b20458d218bb11
Alex Bewley <bewley@robots.ox.ac.uk>
2017-07-10 17:01:50 +0100

    Added GPL license

---
+++ hart/data/kitti/__init__.py
@@ -0,0 +1,20 @@
+########################################################################################
+# 
+# Hierarchical Attentive Recurrent Tracking
+# Copyright (C) 2017  Adam R. Kosiorek, Oxford Robotics Institute, University of Oxford
+# email:   adamk@robots.ox.ac.uk
+# webpage: http://ori.ox.ac.uk
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+# 
+########################################################################################
